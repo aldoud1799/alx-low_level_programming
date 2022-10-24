8245a22d301b2e045f50d4083c09c525143267fb
Abdullah Abbas <abdallah.abbas.jr@gmail.com>
2022-10-24 15:23:40 -0700

    new 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
- * print_rev - a function that prints a string, in reverse
- * fcounter is to first count to end, n is to count back
- * @s: str input
- * Return: string in reverse
- */
+* print_rev - a function that prints a string, in reverse
+* fcounter is to first count to end, n is to count back
+* @s: str input
+* Return: string in reverse
+*/
 void print_rev(char *s)
 {
 	int fcounter = 0;
