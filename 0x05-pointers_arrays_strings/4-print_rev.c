dc670e1e8104dd373581fdd626c03fcba6d84fbb
Abdullah Abbas <abdallah.abbas.jr@gmail.com>
2022-10-24 15:18:10 -0700

    4-1

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,12 +13,12 @@ void print_rev(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{	
-	fcounter++;
+		fcounter++;
 	}
 
 	for (n = (fcounter - 1); n >= 0; n--)
 	{
-	_putchar(s[n]);
+		_putchar(s[n]);
 	}
 	_putchar('\n');
 }
