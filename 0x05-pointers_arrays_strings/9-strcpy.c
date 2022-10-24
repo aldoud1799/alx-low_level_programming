b4e1eac847edc13d070073c1c4678b0f09488276
Abdullah Abbas <abdallah.abbas.jr@gmail.com>
2022-10-24 16:04:51 -0700

    9n

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,13 +8,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int i;
-i = 0;
+	int i = 0;
+	
 	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-dest[i] = '\0';
-return (dest);
+	dest[i] = '\0';
+	return (dest);
 } 
