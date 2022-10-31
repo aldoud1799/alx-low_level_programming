d6c8cd3b5512fd69e58bed5231991383fd45e15f
Abdullah Abbas <abdallah.abbas.jr@gmail.com>
2022-10-31 15:48:42 -0700

    9

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
 
 /**
- * set_string - Entry point
- * @s: input
- * @to: input
- * Return: Always 0 (Success)
+ * set_string - sets the value of a pointer to a char
+ *
+ * @s: The pointer to change
+ * @to: The value to change the pointer to
+ *
+ * Return: nothing
  */
 void set_string(char **s, char *to)
 {
