a17f183e9da2a1a0f6c9a862961c95b9b51e03fd
Abdullah Abbas <abdallah.abbas.jr@gmail.com>
2022-11-05 12:37:45 -0700

    3-3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (0);
 	}
+
 	else if (argc == 3)
 	{
 		for (i = 0 ; i < argc ; i++)
@@ -35,6 +36,7 @@ int main(int argc, char *argv[])
 	mult = j * k;
 	printf("%d\n", mult);
 	}
+
 	return (0);
 }
 	
